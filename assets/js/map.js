c7939d11b60fb7bc58834482a9b3ce4f420d9502
nobelreg <regula.nobel@srf.ch>
2023-04-13 18:56:35 +0200

    new token

--- assets/js/map.js
+++ assets/js/map.js
@@ -91,7 +91,7 @@ export const loadMap = () => {
       }
     }); */
     
-  const tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  const tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWViYmVlcmkiLCJhIjoiY2xnZmJoNDBtMDAyYjNmbjhhY2cxMTh1YiJ9.qI3inodE76upQLtPEaNgrA', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
